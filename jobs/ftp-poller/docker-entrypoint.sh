709d798e8bb146c3dc4b0e6dd82f9c46f27dc822
Jia Xu <Jia.Xu@gov.bc.ca>
2026-04-24 16:28:45 -0700

    fix(ftp-poller): fix CrashLoopBackOff caused by bookworm glibc /etc/passwd lookup

--- jobs/ftp-poller/docker-entrypoint.sh
+++ jobs/ftp-poller/docker-entrypoint.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Fix for OpenShift random UID - bookworm's glibc is stricter about /etc/passwd lookups
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "pay:x:$(id -u):0:pay:/ftp-poller:/bin/bash" >> /etc/passwd
+    echo "Added UID $(id -u) to /etc/passwd"
+  fi
+fi
+
 function start_cron_jobs() {
   echo "Starting go-crond as a background task ..."
   CRON_CMD="go-crond -v --allow-unprivileged --include=cron/"
